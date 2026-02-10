38e646118acc34e0f4e72714c26de526d09047a5
Ujjwal Kumar <uk727517@gmail.com>
2026-02-10 21:51:56 +0530

    class

---
+++ Array/array.java
@@ -0,0 +1,11 @@
+public class StringArray {
+    public static void main(String[] args) {
+
+        String[] names = {"Ram", "Shyam", "Mohan", "Sita"};
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
+    }
+}
